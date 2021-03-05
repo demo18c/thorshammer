53140f2bd9bb04bc7fd82c9eb71ba156e8dd6c54
RASHAD ABDULBARR <rashadabdulbarr@RASHADs-MacBook-Pro.local>
2021-03-05 16:32:59 -0500

    amplify

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { Component } from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Appbar from "./components/Appbar.js";
-import Routes from "./routes/index";
-import "./App.css";
+import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Appbar from './components/Appbar.js';
+import Routes from './routes/index';
 
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+import './App.css';
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+Amplify.configure(awsExports);
 
 const App = () => {
-    const palletType = "dark";
-    const darkTheme = createMuiTheme({
-        palette: {
-            type: palletType,
-        },
-    });
+	const palletType = 'dark';
+	const darkTheme = createMuiTheme({
+		palette: {
+			type: palletType
+		}
+	});
 
-    return (
-        <ThemeProvider theme={darkTheme}>
-            <>
-                {/* <CssBaseline /> */}
-                <Appbar />
-                <Routes />
-            </>
-        </ThemeProvider>
-    );
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<>
+				{/* <CssBaseline /> */}
+				<Appbar />
+				<Routes />
+			</>
+		</ThemeProvider>
+	);
 };
 
-export default App;
+export default withAuthenticator(App);
